a6843c6067d66f71b557de6e0c77130356d09b4e
Christian Schulze <christian.schulze@mni.thm.de>
2016-05-19 13:08:47 +0200

    improved link to local instance

--- index.js
+++ index.js
@@ -93,7 +93,7 @@
     });
 
     http.listen(app.get('port'), function() {
-      console.log('listening on port ' + app.get('port'));
+      console.log('listening on http://localhost:' + app.get('port'));
     });
 
 })();
