a9b3ad2302c7369fe222b0edc4f85bdf466b4702
Christian Schulze <christian.schulze@mni.thm.de>
2016-09-04 16:56:41 +0200

    removed main.js; introduced singular index.js for index page

---
+++ frontend/public/js/index.js
@@ -0,0 +1,80 @@
+(function() {
+    'use strict';
+
+    var socket = io(),
+        request = window.superagent;
+
+    Vue.config.delimiters = ['${', '}'];
+
+    function _deleteExperiment(expId) {
+        request
+            .del('/api/v1/experiment/' + expId)
+            .end(function(res) {
+                console.log(res);
+            });
+    }
+
+    /**
+    * TODO:
+    *    * test all the socket emits
+    *    * working: experiment-added
+    */
+    var vm = new Vue({
+        el: '#vue-instance',
+        data: {
+            experiments: [],
+            experimentColumns: ['date_added', 'name']
+        },
+        methods: {
+            deleteExperiment: _deleteExperiment
+        },
+        created: function() {
+            socket.on('experiment-removed', function(data) {
+                console.log('experiment-removed');
+
+                for (var i = 0; i < this.experiments.length; i++) {
+                    if (this.experiments[i]._id === data._id) {
+                        this.experiments.$remove(this.experiments[i]);
+                        return;
+                    }
+                }
+            }.bind(this));
+
+            socket.on('experiment-added', function(data) {
+                console.log('experiment-added');
+                console.log(data);
+
+                this.experiments.unshift(data.data);
+            }.bind(this));
+
+            socket.on('model-added', function(model) {
+                console.log('model-added');
+
+                for (var i = 0; i < this.experiments.length; i ++) {
+                    if (this.experiments[i]._id == model._id) {
+                        this.experiments[i].models.push(model);
+                    }
+                }
+            }.bind(this));
+
+            socket.on('model-removed', function(data) {
+                console.log('model-removed');
+                console.log(data);
+
+                for (var i = 0; i < this.experiments.length; i++) {
+                    if (this.experiments[i]._id == data.experiment_id) {
+                        console.log(this.experiments[i]);
+
+                        for (var j = 0; j < this.experiments[i].models.length; j++) {
+                            if (this.experiments[i].models[j]._id == data._id) {
+                                this.experiments[i].models.$remove(
+                                    this.experiments[i].models[j]
+                                );
+                            }
+                        }
+                    }
+                }
+            }.bind(this));
+        }
+    });
+})();
